b1e1bb16ae7fa418600a15ad29658fc8a8e5b314
Chenrui.He <hechenrui123@gmail.com>
2019-01-05 11:46:44 +0800

    init Commit.

---
+++ simplehttpserver/simplehttpserver.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+        "fmt"
+        "net"
+        "net/http"
+        "os"
+)
+
+var ip string
+
+func GetLocalIP() {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+		fmt.Println("ERROR:" + err.Error())
+		os.Exit(1)
+	}
+    for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+                ip = ipnet.IP.String()
+			}
+		}
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<h1>Server IP: <font color=blue>%s</font>, Client IP: %s.</h1>", ip, r.RemoteAddr)
+}
+
+func main() {
+    GetLocalIP()
+    fmt.Println(ip)
+	http.HandleFunc("/", handler)
+    http.ListenAndServe("0.0.0.0:80", nil)
+}
+
